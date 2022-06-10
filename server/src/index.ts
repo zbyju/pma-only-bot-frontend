9216a44fbf6ef595f3eef77df7783fc2d54cf9cb
zbyju <zbynek.jurica@gmail.com>
2022-06-11 00:09:12 +0200

    Add routes to node.js server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,19 @@
-function main() {
-  console.log('Hello');
-}
+import express from 'express';
 
-main();
+const app = express();
+
+// Routes
+const apiPrefix = '/v1/';
+
+const generalStats = require('./routes/stats/general');
+const serverStats = require('./routes/stats/server');
+const userStats = require('./routes/stats/user');
+
+app.use(apiPrefix + 'stats/general', generalStats);
+app.use(apiPrefix + 'stats/server', serverStats);
+app.use(apiPrefix + 'stats/user', userStats);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Listening on port: ' + port);
+});
---
+++ server/src/routes/stats/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello from server');
+});
+
+module.exports = router;
