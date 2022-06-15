1d4cfcc493c67b24633b66662044d9f8d1afd52d
zbyju <zbynek.jurica@gmail.com>
2022-06-15 19:33:25 +0200

    Add types for stats

---
+++ server/src/types/stats/general.types.ts
@@ -0,0 +1,9 @@
+export interface GeneralStats {
+  counts: {
+    servers: number;
+    channels: number;
+    users: number;
+    messages: number;
+    emotes: number;
+  };
+}
---
+++ server/src/types/stats/server.types.ts
@@ -0,0 +1,40 @@
+export type ServerId = string;
+export type ChannelId = string;
+export type EmoteId = string;
+export type UserId = string;
+
+export interface DayStats extends DayStatsHeaders {
+  perUser: StatsPerUserPerDay[];
+  server: ServerStatsPerDay;
+}
+
+export interface DayStatsHeaders {
+  serverId: ServerId;
+  date: Date;
+}
+
+export interface ServerStatsPerDay {
+  perChannel: StatsPerChannel[];
+  count: number;
+}
+
+export interface StatsPerChannel {
+  channelId: ChannelId;
+  count: number;
+}
+
+export interface StatsPerUserPerDay {
+  user: UserId;
+  emotes: EmoteCount[];
+  channels: ChannelCount[];
+}
+
+export interface EmoteCount {
+  emoteId: EmoteId;
+  count: number;
+}
+
+export interface ChannelCount {
+  channelId: ChannelId;
+  count: number;
+}
