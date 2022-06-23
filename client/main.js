c98f0ab1aa0e9bcb55ecff28af51d62bdb86c9c2
zbyju <zbynek.jurica@gmail.com>
2022-06-23 14:30:25 +0200

    Add baseurl to flags

--- client/main.js
+++ client/main.js
@@ -2,4 +2,9 @@ import "./style.css";
 import { Elm } from "./src/Main.elm";
 
 const root = document.querySelector("#app div");
-const app = Elm.Main.init({ node: root });
+const app = Elm.Main.init({
+    node: root,
+    flags: {
+        baseApiUrl: "http://localhost:3001/",
+    }
+});
