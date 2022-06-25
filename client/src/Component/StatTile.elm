f439122f29a9713faf2358e444ede948f380a77f
zbyju <zbynek.jurica@gmail.com>
2022-06-25 18:32:29 +0200

    Refactor stattile to enable different data types

--- client/src/Component/StatTile.elm
+++ client/src/Component/StatTile.elm
@@ -1,4 +1,4 @@
-module Component.StatTile exposing (view)
+module Component.StatTile exposing (ValueStatTile(..), view)
 
 import Element
 import Element.Background as Background
@@ -8,10 +8,44 @@ import Simple.Transition as Transition
 import Style.Color as Color
 
 
-view : String -> Float -> Element.Element msg
+type ValueStatTile
+    = StringStatTile String
+    | IntStatTile Int
+    | FloatStatTile Float
+    | UrlStatTile String String
+
+
+view : String -> ValueStatTile -> Element.Element msg
 view label value =
+    let
+        valueElement =
+            case value of
+                StringStatTile str ->
+                    Element.text str
+
+                IntStatTile int ->
+                    Element.text <| String.fromInt int
+
+                FloatStatTile float ->
+                    Element.text <| String.fromFloat float
+
+                UrlStatTile url desc ->
+                    Element.image
+                        [ Element.width <| Element.px 36
+                        , Element.height <| Element.px 36
+                        ]
+                        { src = url, description = desc }
+
+        ( ( paddingT, paddingB ), ( paddingL, paddingR ) ) =
+            case value of
+                UrlStatTile _ _ ->
+                    ( ( 4, 10 ), ( 12, 12 ) )
+
+                _ ->
+                    ( ( 10, 10 ), ( 12, 12 ) )
+    in
     Element.column
-        [ Element.paddingXY 10 5
+        [ Element.paddingEach { top = paddingT, bottom = paddingB, left = paddingL, right = paddingR }
         , Background.color Color.accentBackground
         , Element.spacingXY 0 5
         , Font.center
@@ -26,11 +60,12 @@ view label value =
             , Font.size 30
             , Element.paddingEach { top = 0, left = 0, right = 0, bottom = 5 }
             ]
-            (Element.text <| String.fromFloat value)
+            valueElement
         , Element.el
             [ Element.centerX
             , Font.center
             , Font.extraLight
+            , Font.size 20
             ]
             (Element.text label)
         ]
