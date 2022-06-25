d41924efc0e4f0b1d3dd947e83900345a1fb1365
zbyju <zbynek.jurica@gmail.com>
2022-06-25 18:28:24 +0200

    Change server to serve last week of data

--- server/src/routes/stats/server.ts
+++ server/src/routes/stats/server.ts
@@ -6,7 +6,9 @@ const router = express.Router();
 
 router.get('/:serverId', (req, res) => {
   if (req.params.serverId?.length === 0) return res.status(400).send('No server ID provided.');
-  const from = req.query.from?.toString() || new Date().toUTCString();
+  const from =
+    req.query.from?.toString() ||
+    new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 7).toUTCString();
   return res.json({ stats: generateDayStatsFrom(from, req.params.serverId) });
 });
 
