1c2f51a08d75e966cce182078b7b1d28310bb9e1
zbyju <zbynek.jurica@gmail.com>
2022-06-26 20:06:27 +0200

    Fix server not being able to respond to requests withCredentials

--- server/src/routes/auth/discord.ts
+++ server/src/routes/auth/discord.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import passport from 'passport';
+import { isAuthenticated } from '../../utils/middlewares';
 const router = express.Router();
 
 router.get('/', passport.authenticate('discord'), (req, res) => {
@@ -8,6 +9,14 @@ router.get('/', passport.authenticate('discord'), (req, res) => {
 
 router.get('/redirect', passport.authenticate('discord'), (req, res) => {
   return res.redirect('http://localhost:3000/');
+});
+
+router.get('/status', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.json({ isAuthenticated: isAuthenticated(req) });
 });
 
 export default router;
--- server/src/utils/middlewares.ts
+++ server/src/utils/middlewares.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
-export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  return req.user ? next() : res.status(403).send('Unauthorized');
+export const isAuthenticatedMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  return isAuthenticated(req) ? next() : res.status(403).send('Unauthorized');
+};
+
+export const isAuthenticated = (req: Request): boolean => {
+  return req.user ? true : false;
 };
